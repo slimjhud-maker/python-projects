0636d024efe2e980386c9a585c85ff27dfa9686b
Jhud <slimjhud@gmail.com>
2025-11-19 20:18:41 +0000

    Added comments by making a new branch and then merged main with comments

--- Class_1_number_guessing.py
+++ Class_1_number_guessing.py
@@ -1,13 +1,13 @@
-import random
-num = random.randint(1,100)
+import random 
+num = random.randint(1,100) # creates range of 1 to 100
 while True:
-    user_num = int(input("Pick a number from 1 to 100: "))
-    if user_num == num:
-        print("Correct! Well Done")
+    user_num = int(input("Pick a number from 1 to 100: ")) # asking user to pick a number
+    if user_num == num: # checking if the user's number is the same as the random number
+        print("Correct! Well Done") 
         break
-    elif user_num > num:
+    elif user_num > num: # checking if the user's number is the larger than the random number 
         print("\nToo high. Guess lower next time\n")
-    elif user_num < num:
+    elif user_num < num: # checking if the user's number is the smaller than the random number
         print("\nToo low. Guess higher next time\n")
     else:
         print("\nPlease input a number\n")
