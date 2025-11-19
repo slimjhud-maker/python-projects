d4a54c9fa1e922ec3142e92e7baa8d05b6e20cc4
Jhud <slimjhud@gmail.com>
2025-11-19 20:30:39 +0000

    I don't know why I didn't just use the pull command for adding comments

--- Class_1_counting_even_numbers.py
+++ Class_1_counting_even_numbers.py
@@ -1,7 +1,7 @@
-count = 0
-num_1 = int(input("Select a number to be the first number in your range: "))
-num_2 = int(input("Select a number to be the last number in your range: "))
-for i in range(num_1,num_2 + 1):
+count = 0 # setting the amount of even numbers to 0
+num_1 = int(input("Select a number to be the first number in your range: ")) # user will input the first number of the range
+num_2 = int(input("Select a number to be the last number in your range: "))  # user will input the last number of the range
+for i in range(num_1 + 1,num_2): # creating the range with range function. Add 1 to num_2 
     if i % 2 == 0:
         count += 1
     else:
